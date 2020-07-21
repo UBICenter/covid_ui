c716ae556615c90bf616e913f1e6abe02fb32e5f
Max Ghenis <mghenis@gmail.com>
2020-07-21 00:45:38 -0700

    Update to Ernie's code

--- tcpov2a_make_taxsim27.R
+++ tcpov2a_make_taxsim27.R
@@ -1,7 +1,8 @@
-# October 3, 2016
-# This file is for reading in the cps data
-# and filtering the variables I need for taxism
-# amended script to do all the processing after binding
+# TCPOVERTY: STEP 2A
+# MAKE TAXSIM27
+# Ernie Tedeschi
+# with modifications to code from Sam Portnow: https://users.nber.org/~taxsim/to-taxsim/cps/cps-portnow/TaxSimRScriptForDan.R
+# Last Updated: 21 March 2019
 
 library(foreign)
 library(plyr)
@@ -11,22 +12,27 @@ library(RCurl)
 library(dplyr)
 library(ggplot2)
 
-# personal exemptions
+# SETTINGS
 
-pexemp <- data.frame(year=1960:2013, pexemp = c(600,600,600,600,600,600,600,600,600,600,
-                                                625,675,750,750,750,750,750,750,750, 1000,
-                                                1000,1000,1000,1000,1000,1040,1080,1900,1950,2000,
-                                                2050,2150,2300,2350,2450,2500,2550,2650,2700,2750,
-                                                2800,2900,3000,3050,3100,3200,3300,3400,3500,3650,
-                                                3750,3750,3750,3750))
-pexemp
-# gotta get 1999 thru 2009
-pexemp <- pexemp[pexemp$year > 1998 & pexemp$year < 2010,]
+## pexemp: Personal exemption for year of analysis
+pexemp <- data.frame(year=2017, pexemp = 4050)
 
+## wd: Working Directory
+wd <- '~/Documents/data/cpsasec/make taxsim'
 
-setwd('~/Dropbox/PhD/Diss/Analyses/ECLS-B')
+## wdtaxsim: Tax-Calculator taxsim directory
+wdtaxsim <- '~/tc/Tax-Calculator/taxcalc/validation/taxsim'
 
-ipum <- read.spss('./CPS/cps01_09.sav', to.data.frame=T, use.missings = T, use.value.labels = F)
+## datafile: Name of ASEC extract file
+datafile <- 'dtadata.csv'
+
+# END SETTINGS 
+
+##################################################
+
+setwd(wd)
+
+ipum <- read.csv(datafile, stringsAsFactors = F)   
 # set to lower case
 names(ipum) <- tolower(names(ipum))
 
@@ -61,9 +67,6 @@ ipum[,vars][ipum[,vars] == 99997] <- 0
 ipum[,vars][ipum[,vars] == 999997] <- 0
 ipum[,vars][ipum[,vars] == 9999997] <- 0
 
-library(psych)
-
-
 # more missing codes
 
 vars <- 'capgain' 
@@ -85,8 +88,6 @@ ipum[,vars][ipum[,vars] == 999997] <- -999
 ipum[,vars][ipum[,vars] == 9999997] <- -999
 
 
-
-
 # set 0's to NA for location
 
 ipum[ipum$momloc == 0,]$momloc <- NA
@@ -100,37 +101,42 @@ ipum$x2 <- ipum$year - 1
 # set x3 to  fips code
 ipum$x3 <- ipum$statefip
 
-# convert to soi
-
-source('FIPStoSOI.R')
-
 # /* Marital status will be sum of spouse's x4 values */
 ipum$x4 <- 1
 
-# if age > 65, x6 is 1
-ipum$x6 <- ifelse(ipum$age > 65, 1, 0)
+# Cohabitators not married
+
+ipum$sploc[ipum$relate == 1114] <- NA
+
+# x6 is just age for now
+ipum$x6 <- ifelse((is.na(ipum$sploc) | (ipum$sploc > 0 & ipum$sploc > ipum$pernum)), ipum$age, 0)
+ipum$x24 <- ifelse((!is.na(ipum$sploc) & ipum$sploc > 0 & ipum$sploc < ipum$pernum), ipum$age, 0)
+
 
 # primary wage or spouse wage
-ipum$x7 <- ifelse(ipum$sex==1, ipum$incwage + ipum$incbus + ipum$incfarm, 0)
-ipum$x8 <- ifelse(ipum$sex==2, ipum$incwage + ipum$incbus + ipum$incfarm, 0)
+ipum$x7 <- ifelse((is.na(ipum$sploc) | (ipum$sploc > 0 & ipum$sploc > ipum$pernum)), ipum$incwage + ipum$incbus + ipum$incfarm, 0)
+ipum$x8 <- ifelse((!is.na(ipum$sploc) & ipum$sploc > 0 & ipum$sploc < ipum$pernum), ipum$incwage + ipum$incbus + ipum$incfarm, 0)
 
 
 ipum$x9 <- ipum$incdivid
-ipum$x10 <- ipum$incint + ipum$incrent + ipum$incother + ipum$incalim
+ipum$x10 <- ipum$incrent + ipum$incother + ipum$incalim
 ipum$x11 <- ipum$incretir
 ipum$x12 <- ipum$incss
+ipum$x27 <- ipum$incint
+ipum$x28 <- 0
 
 # /* Commented out got* items below because they are an error - 
 # hope to fix soon. drf, 
 # Nov18, 2015
 # */
+
 attach(ipum)
 ipum$x13 <- incwelfr+incwkcom+incvet+incsurv+incdisab+incchild+inceduc+
-  # /*gotveduc+gotvothe+gotvpens+gotvsurv+*/ 
-  incssi+incasist
+        # /*gotveduc+gotvothe+gotvpens+gotvsurv+*/ 
+        incssi+incasist
 detach(ipum)
 
-ipum$x14 <- 0
+ipum$x14 <- ipum$incrent
 ipum$x15 <- 0
 
 
@@ -162,6 +168,7 @@ ipum$capgain[ipum$capgain==-999] <- 0
 # dependent on the household head (which may be incorrect) and non-relatives
 # are separate tax units. 
 # */
+
 attach(ipum)
 
 ipum$hnum <- 0
@@ -175,8 +182,15 @@ ipum$sum <- rowSums(ipum[,c('x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x22')
 
 ipum$notself <- ifelse(rowSums(ipum[,c('x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x22')], na.rm=T) <= ipum$pexemp, 1, 0)
 
-ipum$deprel <- ifelse(ipum$notself==1 & ipum$relate >= 303 & ipum$relate <= 901, 1, 0)
-ipum$depchild <- ifelse(ipum$notself==1 & rowSums(ipum[,c('momloc', 'poploc')], na.rm=T) > 0 & (ipum$age < 18 | ipum$age < 24 & ipum$schlcoll > 0), 1, 0)
+ipum$depstat[!is.na(ipum$sploc) & ipum$depstat > 0 & ipum$depstat == ipum$sploc] <- 0
+
+
+ipum$depchild <- ifelse(ipum$depstat > 0 & (!is.na(ipum$momloc) | !is.na(ipum$poploc)) & (ipum$age < 18 | (ipum$age < 24 & ipum$schlcoll > 0)), 1, 0)
+ipum$deprel <- ifelse(ipum$depstat > 0 & ipum$depchild == 0, 1, 0)
+ipum$dep13 <- ifelse((ipum$deprel == 1 | ipum$depchild == 1) & ipum$age < 13, 1, 0)
+ipum$dep17 <- ifelse((ipum$deprel == 1 | ipum$depchild == 1) & ipum$age < 17, 1, 0)
+ipum$dep18 <- ifelse((ipum$deprel == 1 | ipum$depchild == 1) & ipum$age < 18, 1, 0)
+
 
 detach(ipum)
 
@@ -188,42 +202,52 @@ dpndnts[dpndnts$deprel == 1,]$x1 <- 100*dpndnts[dpndnts$deprel == 1,]$serial + d
 
 dpndnts$x4 <- NA
 dpndnts$x5 <- 1
-dpndnts$x6 <- NA
+dpndnts$x6 <- 0
 dpndnts$x19 <- NA
+dpndnts$x23 <- NA
+dpndnts$x24 <- 0
 
 txpyrs <- ipum[ipum$depchild == 0 & ipum$deprel == 0,]
 txpyrs$x1 <- 0
 txpyrs$x1 <- 100*txpyrs$serial + apply(txpyrs[,c('pernum', 'sploc')], 1, min, na.rm=T)
 
 txpyrs$x5 <- 0
-
+txpyrs$x23 <- NA
 
 # set whats not x1, x2, or x5 in deps to NA
-vars <- c('x3', paste0('x', 4), paste0('x', 6:22))
+vars <- c('x3', paste0('x', 4), paste0('x', 7:23), 'x27', 'x28')
 dpndnts[,vars] <- NA
 
 # put them back together
 ipum <- rbind(txpyrs, dpndnts)
 
 
-library(dplyr)
 # sum value over tax #
 concat <- group_by(ipum, x2, x1)
-
-concat <- summarise(concat, x3 = mean(x3, na.rm=T), x4 = sum(deprel, na.rm=T) + sum(depchild, na.rm=T),
-                    x5 = sum(x5, na.rm=T), x6 = sum(x6, na.rm=T), x7 = sum(x7, na.rm=T), x8 = sum(x8, na.rm=T), x9 = sum(x9, na.rm=T),
+concat <- summarise(concat, x3 = mean(x3, na.rm=T), x4 = sum(x4, na.rm=T),
+                    x5 = sum(x5, na.rm=T), x6 = max(x6, na.rm=T), x7 = sum(x7, na.rm=T), x8 = sum(x8, na.rm=T), x9 = sum(x9, na.rm=T),
                     x10 = sum(x10, na.rm=T), x11 = sum(x11, na.rm=T), x12 = sum(x12, na.rm=T), x13 = sum(x13, na.rm=T), x14 = sum(x14, na.rm=T), x15 = sum(x15, na.rm=T),
-                    x16 = sum(x16, na.rm=T), x17 = sum(x17, na.rm=T), x18 = sum(x18, na.rm=T), x19 = sum(depchild, na.rm=T), x20 = sum(x20, na.rm=T), x21 = sum(x21, na.rm=T), x22 = sum(x22, na.rm=T))
+                    x16 = sum(x16, na.rm=T), x17 = sum(x17, na.rm=T), x18 = sum(x18, na.rm=T), x19 = sum(depchild, na.rm=T), x20 = sum(x20, na.rm=T), x21 = sum(x21, na.rm=T), x22 = sum(x22, na.rm=T), 
+                    x23 = sum(dep13, na.rm=T), x24 = max(x24, na.rm=T), x25 = sum(dep17, na.rm=T), x26 = sum(dep18, na.rm=T), x27 = sum(x27, na.rm=T), x28 = sum(x28, na.rm=T), x29 = min(serial), x30 = min(pernum))
 
 concat <- data.frame(concat)
 
-concat[! is.na(concat$x4) & concat$x4 == 1 & concat$x5 > 0,]$x4 <- 3
-
-concat <- concat[concat$x19 > 0,]
+concat <- concat[concat$x19 >= 0,]
 concat <- concat[concat$x4 > 0, ]
-vars <- paste0('x', 1:22)
+vars <- paste0('x', 1:30)
 
 concat <- concat[,vars]
 
-names(concat) <- c('id', 'year', 'state', 'mstat', 'depx', 'agex', 'pwages', 'swages', 'dividends', 'otherprop', 'pensions', 'gssi', 'transfers', 'rentpaid', 'proptax', 
-                   'otheritem', 'childcare', 'ui', 'depchild', 'mortgage', 'stcg', 'ltcg')
+names(concat) <- c('taxsimid', 'year', 'state', 'mstat', 'depx', 'page', 'pwages', 'swages', 'dividends', 'otherprop', 'pensions', 'gssi', 'transfers', 'rentpaid', 'proptax', 
+                   'otheritem', 'childcare', 'ui', 'depchild', 'mortgage', 'stcg', 'ltcg', 'dep13', 'sage', 'dep17', 'dep18', 'intrec', 'nonprop', 'serial', 'pernum')
+
+ids <- data.frame(col1=concat$taxsimid, col2=concat$serial, col3=concat$pernum)
+colnames(ids) <- c('RECID', 'serial', 'pernum')
+
+concat <- concat[,c(1:4,6,24,5,23,25,26,7:9,27,21,22,10,28,11:12,18,13:17,20)]
+
+setwd(wdtaxsim)
+
+write.table(concat, sep = " ", file = "c17_taxsim", row.names = F, col.names = F)
+
+write.table(ids, sep = ",", file = "ids.csv", row.names = F, col.names = T)
