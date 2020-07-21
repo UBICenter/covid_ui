f237661441ac08dfd51730176f74f139873ebce0
Max Ghenis <mghenis@gmail.com>
2020-07-21 00:29:27 -0700

    Finish Portnow script in Python - no syntax errors

--- make_tax_units.py
+++ make_tax_units.py
@@ -8,8 +8,8 @@ import pandas as pd
 
 # personal exemptions
 
-pexemp <- pd.DataFrame({
-  'year': np.arange(1960, 2014)
+pexemp = pd.DataFrame({
+  'year': np.arange(1960, 2014),
   'pexemp': [600,600,600,600,600,600,600,600,600,600,625,675,750,750,750,750,
              750,750,750, 1000,1000,1000,1000,1000,1000,1040,1080,1900,1950,
              2000,2050,2150,2300,2350,2450,2500,2550,2650,2700,2750,2800,2900,
@@ -31,8 +31,8 @@ VARS2 = ['fedtax', 'statetax', 'adjginc', 'taxinc', 'fedtaxac', 'fica',
          'incalim', 'incasist', 'incss', 'incwelfr', 'incwkcom', 'incvet',
          'incchild', 'incunemp', 'inceduc', 'gotveduc', 'gotvothe', 'gotvpens',
          'gotvsurv', 'incssi']
-VARS3 <- ['incwage', 'incbus', 'incfarm', 'incsurv', 'incdisab', 'incretir']
-vars <- VARS1 + VARS2 + VARS3 + ['capgain']
+VARS3 = ['incwage', 'incbus', 'incfarm', 'incsurv', 'incdisab', 'incretir']
+vars = VARS1 + VARS2 + VARS3
 
 
 # these are the missing codes
@@ -54,13 +54,13 @@ for col in COLS_ZERO_TO_NA:
 ipum['x2'] = ipum.year - 1
 
 # set x3 to  fips code
-ipum['x3'] <- ipum.statefip
+ipum['x3'] = ipum.statefip
 
 # convert to soi - TODO
 # source('FIPStoSOI.R')
 
 # /* Marital status will be sum of spouse's x4 values */
-ipum['x4' = 1
+ipum['x4'] = 1
 
 # if age > 65, x6 is 1
 ipum['x6'] = np.where(ipum.age > 65, 1, 0)
@@ -80,7 +80,7 @@ ipum['x12'] = ipum.incss
 # hope to fix soon. drf, 
 # Nov18, 2015
 # */
-ipum['x13'] <- ipum[['incwelfr', 'incwkcom', 'incvet', 'incsurv', 'incdisab',
+ipum['x13'] = ipum[['incwelfr', 'incwkcom', 'incvet', 'incsurv', 'incdisab',
                      'incchild', 'inceduc', 'incssi', 'incasist']].sum(axis=1)
   # /*gotveduc+gotvothe+gotvpens+gotvsurv+*/ 
 
@@ -90,23 +90,24 @@ ipum['x15'] = 0
 
 # /* use Census imputation of itemized deductions where available.*/
 # first have to join the exemption table
-pexemp[1] <- 'x2'
-ipum <- join(ipum, pexemp, by='x2')
+pexemp.rename(columns={'year': 'x2'}, inplace=True)
+ipum = ipum.merge(pexemp, on='x2')
 
 # adjusted gross - taxes + exemptions
-ipum$x16 <- ipum$adjginc - rowSums(ipum[,c('pexemp', 'proptax', 'statetax', 'taxinc')], na.rm=T)
+ipum['x16'] = (ipum.adjginc - 
+    ipum[['pexemp', 'proptax', 'statetax', 'taxinc']].sum(axis=1))
 # no values less than 0
-ipum$x16 <- ifelse(ipum$x16 < 0, 0, ipum$x16)
+ipum['x16'] = np.where(ipum.x16 < 0, 0, ipum.x16)
 
-ipum$x17 <- 0
-ipum$x18 <- ipum$incunemp
-ipum$x19 <- 0
-ipum$x20 <- 0
-ipum$x21 <- 0
+ipum['x17'] = 0
+ipum['x18'] = ipum.incunemp
+ipum['x19'] = 0
+ipum['x20'] = 0
+ipum['x21'] = 0
 
 # * Assume capgain and caploss are long term;
-ipum$x22 <- ifelse(! ipum$capgain==-999, ipum$capgain - ipum$caploss, 0)
-ipum$capgain[ipum$capgain==-999] <- 0
+ipum['x22'] = np.where(ipum.capgain != -999, ipum.capgain - ipum.caploss, 0)
+ipum.capgain = np.where(ipum.capgain == -999, 0)
 
 
 # Here we output a record for each person, so that tax units can be formed 
@@ -116,68 +117,65 @@ ipum$capgain[ipum$capgain==-999] <- 0
 # dependent on the household head (which may be incorrect) and non-relatives
 # are separate tax units. 
 # */
-attach(ipum)
 
-ipum$hnum <- 0
-ipum[ipum$relate==101,]$hnum <- ipum[ipum$relate==101,]$pernum
-ipum[ipum$hnum==0,]$hnum <- NA
+ipum['hnum'] = 0
+ipum.hnum = np.where(ipum.relate==101, ipum.pernum, ipum.hnum)
+ipum.hnum.replace(0, np.nan)
 
 # if claiming > personal exemption than they're their own filer
-ipum$notself <- ifelse((ipum$x7 + ipum$x8 + ipum$x9 + ipum$x10 + ipum$x11 + ipum$x12 + ipum$x13 + ipum$x22) <= ipum$pexemp, 1, 0)
+ipum['sum'] = ipum[['x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13',
+                    'x22']].sum(axis=1)
+ipum['notself'] = np.where(ipum.sum <= ipum.pexemp, 1, 0)
 
-ipum$sum <- rowSums(ipum[,c('x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x22')], na.rm=T)
 
-ipum$notself <- ifelse(rowSums(ipum[,c('x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x22')], na.rm=T) <= ipum$pexemp, 1, 0)
-
-ipum$deprel <- ifelse(ipum$notself==1 & ipum$relate >= 303 & ipum$relate <= 901, 1, 0)
-ipum$depchild <- ifelse(ipum$notself==1 & rowSums(ipum[,c('momloc', 'poploc')], na.rm=T) > 0 & (ipum$age < 18 | ipum$age < 24 & ipum$schlcoll > 0), 1, 0)
-
-detach(ipum)
+ipum['deprel'] = np.where((ipum.notself == 1) & (ipum.relate >= 303) & 
+                          (ipum.relate <= 901), 1, 0)
+ipum['depchild'] = np.where(
+    (ipum.notself == 1) & 
+    (ipum[['momloc', 'poploc']].sum(axis=1) > 0) &
+    ((ipum.age < 18) | (ipum.age < 24 & ipum.schlcoll > 0)),
+    1, 0)
 
 # set dependents and taxpayers
-dpndnts <- ipum[ipum$depchild==1 | ipum$deprel==1,]
-dpndnts$x1 <- 0
-dpndnts[dpndnts$depchild == 1,]$x1 <- 100*dpndnts[dpndnts$depchild == 1,]$serial + apply(dpndnts[dpndnts$depchild == 1,][,c('momloc', 'poploc')], 1, min, na.rm=T)
-dpndnts[dpndnts$deprel == 1,]$x1 <- 100*dpndnts[dpndnts$deprel == 1,]$serial + dpndnts[dpndnts$deprel==1,]$hnum
+dpndnts = ipum[(ipum.depchild == 1) | (ipum.deprel == 1)]
+dpndnts['x1'] = np.where(dpndnts.depchild == 1,
+    100 * dpndnts.serial + np.minimum(dpndnts.momloc, dpndnts.poploc), 0)
+dpndnts.x1 = np.where(dpndnts.deprel == 1,
+    100 * dpndnts.serial + dpndnts.hnum, dpndnts.x1)
 
-dpndnts$x4 <- NA
-dpndnts$x5 <- 1
-dpndnts$x6 <- NA
-dpndnts$x19 <- NA
+dpndnts['x4'] = np.nan
+dpndnts['x5'] = 1
+dpndnts['x6'] = np.nan
+dpndnts['x19'] = np.nan
 
-txpyrs <- ipum[ipum$depchild == 0 & ipum$deprel == 0,]
-txpyrs$x1 <- 0
-txpyrs$x1 <- 100*txpyrs$serial + apply(txpyrs[,c('pernum', 'sploc')], 1, min, na.rm=T)
-
-txpyrs$x5 <- 0
+txpyrs = ipum[(ipum.depchild == 0) & (ipum.deprel == 0)]
+txpyrs.x1 = 100 * txpyrs.serial + np.minimum(txpyrs.pernum, txpyrs.sploc)
+txpyrs.x5 = 0
 
 
 # set whats not x1, x2, or x5 in deps to NA
-vars <- c('x3', paste0('x', 4), paste0('x', 6:22))
-dpndnts[,vars] <- NA
+vars = ['x' + str(i) for i in [3, 4] + range(6, 23)]
+dpndnts[vars] = np.nan
 
 # put them back together
-ipum <- rbind(txpyrs, dpndnts)
+ipum = pd.concat([txpyrs, dpndnts])
 
 
-library(dplyr)
 # sum value over tax #
-concat <- group_by(ipum, x2, x1)
+ipum['n'] = 1
+concat = ipum.group_by(['x2', 'x1'])[['n', 'deprel', 'depchild'] +
+                                     ['x' + str(i) for i in range(5, 23)]].sum()
+concat['x3'] = concat.x3 / concat.n
+concat['x4'] = concat.deprel + concat.depchild
 
-concat <- summarise(concat, x3 = mean(x3, na.rm=T), x4 = sum(deprel, na.rm=T) + sum(depchild, na.rm=T),
-                    x5 = sum(x5, na.rm=T), x6 = sum(x6, na.rm=T), x7 = sum(x7, na.rm=T), x8 = sum(x8, na.rm=T), x9 = sum(x9, na.rm=T),
-                    x10 = sum(x10, na.rm=T), x11 = sum(x11, na.rm=T), x12 = sum(x12, na.rm=T), x13 = sum(x13, na.rm=T), x14 = sum(x14, na.rm=T), x15 = sum(x15, na.rm=T),
-                    x16 = sum(x16, na.rm=T), x17 = sum(x17, na.rm=T), x18 = sum(x18, na.rm=T), x19 = sum(depchild, na.rm=T), x20 = sum(x20, na.rm=T), x21 = sum(x21, na.rm=T), x22 = sum(x22, na.rm=T))
+concat.x4 = np.where(~concat.x4.isna() & (concat.x4 == 1) & (concat.x5 > 0), 3,
+                     concat.x4)
 
-concat <- data.frame(concat)
+concat = concat[(concat.x19 > 0) & (concat.x4) > 0]
 
-concat[! is.na(concat$x4) & concat$x4 == 1 & concat$x5 > 0,]$x4 <- 3
+concat = concat[['x' + str(i) for i in range(1, 23)]]
 
-concat <- concat[concat$x19 > 0,]
-concat <- concat[concat$x4 > 0, ]
-vars <- paste0('x', 1:22)
-
-concat <- concat[,vars]
-
-names(concat) <- c('id', 'year', 'state', 'mstat', 'depx', 'agex', 'pwages', 'swages', 'dividends', 'otherprop', 'pensions', 'gssi', 'transfers', 'rentpaid', 'proptax', 
-                   'otheritem', 'childcare', 'ui', 'depchild', 'mortgage', 'stcg', 'ltcg')
+concat.columns = ['id', 'year', 'state', 'mstat', 'depx', 'agex', 'pwages',
+                  'swages', 'dividends', 'otherprop', 'pensions', 'gssi',
+                  'transfers', 'rentpaid', 'proptax', 'otheritem', 'childcare',
+                  'ui', 'depchild', 'mortgage', 'stcg', 'ltcg']
