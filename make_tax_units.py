b90c72bc7f6b3975b45ad80769543aa63b2a2010
Max Ghenis <mghenis@gmail.com>
2020-07-21 01:20:19 -0700

    Update per Ernie's revisions

--- make_tax_units.py
+++ make_tax_units.py
@@ -9,11 +9,8 @@ import pandas as pd
 # personal exemptions
 
 pexemp = pd.DataFrame({
-  'year': np.arange(1960, 2014),
-  'pexemp': [600,600,600,600,600,600,600,600,600,600,625,675,750,750,750,750,
-             750,750,750, 1000,1000,1000,1000,1000,1000,1040,1080,1900,1950,
-             2000,2050,2150,2300,2350,2450,2500,2550,2650,2700,2750,2800,2900,
-             3000,3050,3100,3200,3300,3400,3500,3650,3750,3750,3750,3750]})
+  'year': [2018],
+  'pexemp': [0]})
 
 # gotta get 1999 thru 2009
 pexemp = pexemp[pexemp.year.between(1998, 2010)]
@@ -25,7 +22,9 @@ ipum.columns = ipum.columns.str.lower()
 
 # /* Set missing income items to zero so that non-filers etc will get zeroes.*/
 # find out what statatax is and get it
-VARS2 = ['fedtax', 'statetax', 'adjginc', 'taxinc', 'fedtaxac', 'fica',
+VARS1 = ['eit_cred', 'fed_ret']
+VARS2 = ['fedtax_ac', 'fedtax_bc', 'statetax_a', 'statetax_b' 'adjginc',
+         'taxinc', 'fedtaxac', 'fica',
          'caploss', 'stataxac', 'incdivid', 'incint', 'incrent', 'incother',
          'incalim', 'incasist', 'incss', 'incwelfr', 'incwkcom', 'incvet',
          'incchild', 'incunemp', 'inceduc', 'gotveduc', 'gotvothe', 'gotvpens',
@@ -58,22 +57,34 @@ ipum['x3'] = ipum.statefip
 # convert to soi - TODO
 # source('FIPStoSOI.R')
 
-# /* Marital status will be sum of spouse's x4 values */
+# Marital status will be sum of spouse's x4 values
 ipum['x4'] = 1
 
-# if age > 65, x6 is 1
-ipum['x6'] = np.where(ipum.age > 65, 1, 0)
+# Cohabitators not married
+ipum.loc[ipum.relate == 1114, 'sploc'] = np.nan
+
+# x6 is just age for now
+ipum['x6'] = np.where(ipum.sploc.isna() | 
+                      ((ipum.sploc > 0) & (ipum.sploc > ipum.pernum)),
+                      ipum.age, 0
+ipum['x24'] = np.where(~ipum.sploc.isna() & (ipum.sploc > 0) & 
+                       (ipum.sploc < ipum.pernum), ipum.age, 0)
+
 
 # primary wage or spouse wage
 ipum['incwagebusfarm'] = ipum[['incwage', 'incbus', 'incfarm']].sum(axis=1)
-ipum['x7'] = np.where(ipum.sex == 1, ipum.incwagebusfarm, 0)
-ipum['x8'] = np.where(ipum.sex == 2, ipum.incwagebusfarm, 0)
+ipum['x7'] = np.where((ipum.sploc.isna() | 
+                      ((ipum.sploc > 0) & (ipum.sploc > ipum.pernum)), 
+                      ipum.incwagebusfarm, 0)
+ipum['x8'] = ipum.incwagebusfarm - ipum.x7
 
 
 ipum['x9'] = ipum.incdivid
-ipum['x10'] = ipum[['incint', 'incrent', 'incother', 'incalim']].sum(axis=1)
+ipum['x10'] = ipum[['incrent', 'incother', 'incalim']].sum(axis=1)
 ipum['x11'] = ipum.incretir
 ipum['x12'] = ipum.incss
+ipum['x27'] = ipum.incint
+ipum['x28'] = 0
 
 # /* Commented out got* items below because they are an error - 
 # hope to fix soon. drf, 
@@ -81,9 +92,8 @@ ipum['x12'] = ipum.incss
 # */
 ipum['x13'] = ipum[['incwelfr', 'incwkcom', 'incvet', 'incsurv', 'incdisab',
                      'incchild', 'inceduc', 'incssi', 'incasist']].sum(axis=1)
-  # /*gotveduc+gotvothe+gotvpens+gotvsurv+*/ 
 
-ipum['x14'] = 0
+ipum['x14'] = ipum.incrent
 ipum['x15'] = 0
 
 
@@ -127,13 +137,24 @@ ipum['sum'] = ipum[['x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13',
 ipum['notself'] = np.where(ipum.sum <= ipum.pexemp, 1, 0)
 
 
-ipum['deprel'] = np.where((ipum.notself == 1) & (ipum.relate >= 303) & 
-                          (ipum.relate <= 901), 1, 0)
+
+ipum.loc[~ipum.sploc.isna() & (ipum.depstat > 0) & (ipum.depstat == ipum.sploc),
+         'depstat'] = 0
+
 ipum['depchild'] = np.where(
-    (ipum.notself == 1) & 
+    (ipum.depstat > 1) &
+    (~ipum.momloc.isna() | ~ipum.poploc.isna())
     (ipum[['momloc', 'poploc']].sum(axis=1) > 0) &
     ((ipum.age < 18) | (ipum.age < 24 & ipum.schlcoll > 0)),
     1, 0)
+
+ipum['deprel'] = np.where((ipum.depstat > 0) & (ipum.depchild == 0), 1, 0)
+ipum['dep13'] = np.where(((ipum.deprel == 1) | (ipum.depchild == 1)) & 
+                         (ipum.age < 13), 1, 0)
+ipum['dep17'] = np.where(((ipum.deprel == 1) | (ipum.depchild == 1)) & 
+                         (ipum$age < 17), 1, 0)
+ipum['dep18'] = np.where(((ipum.deprel == 1) | (ipum.depchild == 1)) & 
+                         (ipum.age < 18), 1, 0)
 
 # set dependents and taxpayers
 dpndnts = ipum[(ipum.depchild == 1) | (ipum.deprel == 1)]
@@ -144,16 +165,19 @@ dpndnts.x1 = np.where(dpndnts.deprel == 1,
 
 dpndnts['x4'] = np.nan
 dpndnts['x5'] = 1
-dpndnts['x6'] = np.nan
+dpndnts['x6'] = 0
 dpndnts['x19'] = np.nan
+dpndnts['x23'] = np.nan
+dpndnts['x24'] = 0
 
 txpyrs = ipum[(ipum.depchild == 0) & (ipum.deprel == 0)]
 txpyrs.x1 = 100 * txpyrs.serial + np.minimum(txpyrs.pernum, txpyrs.sploc)
 txpyrs.x5 = 0
+txpyrs.x23 = np.nan
 
 
 # set whats not x1, x2, or x5 in deps to NA
-vars = ['x' + str(i) for i in [3, 4] + range(6, 23)]
+vars = ['x' + str(i) for i in [3, 4, 27, 28] + range(6, 23)]
 dpndnts[vars] = np.nan
 
 # put them back together
@@ -162,19 +186,27 @@ ipum = pd.concat([txpyrs, dpndnts])
 
 # sum value over tax #
 ipum['n'] = 1
-concat = ipum.group_by(['x2', 'x1'])[['n', 'deprel', 'depchild'] +
-                                     ['x' + str(i) for i in range(5, 23)]].sum()
-concat['x3'] = concat.x3 / concat.n
-concat['x4'] = concat.deprel + concat.depchild
+concat_sum = ipum.group_by(['x2', 'x1'])[
+    ['n'] + ['x' + str(i) for i in range(3, 29)]].sum()
+concat_sum.x3 /= concat_sum.n
+# x6 and x24 should be max not sum, and n is no longer necessary.
+concat_sum.drop(['x6', 'x24', 'n'], axis=1, inplace=True)
 
-concat.x4 = np.where(~concat.x4.isna() & (concat.x4 == 1) & (concat.x5 > 0), 3,
-                     concat.x4)
+concat_max = ipum.group_by(['x2', 'x1'])['x6', 'x24'].max()
+concat_min = ipum.group_by(['x2', 'x1'])['serial', 'pernum'].min()
+concat_min.columns = ['x29', 'x30']
 
-concat = concat[(concat.x19 > 0) & (concat.x4) > 0]
+concat = concat_sum.join(concat_max).join(concat_min)
 
-concat = concat[['x' + str(i) for i in range(1, 23)]]
+concat = concat[(concat.x19 >= 0) & (concat.x4) > 0]
 
-concat.columns = ['id', 'year', 'state', 'mstat', 'depx', 'agex', 'pwages',
-                  'swages', 'dividends', 'otherprop', 'pensions', 'gssi',
-                  'transfers', 'rentpaid', 'proptax', 'otheritem', 'childcare',
-                  'ui', 'depchild', 'mortgage', 'stcg', 'ltcg']
+concat = concat[['x' + str(i) for i in range(1, 31)]]
+
+concat.columns = [('taxsimid', 'year', 'state', 'mstat', 'depx', 'page',
+                   'pwages', 'swages', 'dividends', 'otherprop', 'pensions',
+                   'gssi', 'transfers', 'rentpaid', 'proptax', 'otheritem',
+                   'childcare', 'ui', 'depchild', 'mortgage', 'stcg', 'ltcg',
+                   'dep13', 'sage', 'dep17', 'dep18', 'intrec', 'nonprop',
+                   'serial', 'pernum')
+
+ids = concat[['taxsimid', 'serial', 'pernum']]
